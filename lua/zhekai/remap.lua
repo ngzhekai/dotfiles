b63dbe36deefc1ba671ce82f6f69cc8ea4b620c0
Ng Zhe Kai <ngzhekai@gmail.com>
2023-05-15 15:33:00 +0800

    fix: 🐛 typo for remap.lua

--- lua/zhekai/remap.lua
+++ lua/zhekai/remap.lua
@@ -1,6 +1,6 @@
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leadert>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -12,27 +12,27 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 
-vim.keymap.set("n", "<leadert>vwm", function ()
+vim.keymap.set("n", "<leader>vwm", function ()
     require("vim-with-me").StartVimWithMe()
 end)
-vim.keymap.set("n", "<leadert>svwm", function ()
+vim.keymap.set("n", "<leader>svwm", function ()
     require("vim-with-me").StopVimWithMe()
 end)
 
 -- greatest remap ever
-vim.keymap.set("x", "<leadert>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- next greatest remap ever: asbjornHaland
-vim.keymap.set("n", "<leadert>y", "\"+y")
-vim.keymap.set("v", "<leadert>y", "\"+y")
-vim.keymap.set("n", "<leadert>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
 
-vim.keymap.set("n", "<leadert>d", "\"_d")
-vim.keymap.set("v", "<leadert>d", "\"_d")
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
 
 -- interchangeable for Control c to acts like Esc when on insert mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<leadert>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
