5cf3bdfc37bc388216e34245664e471d9a8209e3
Ng Zhe Kai <ngzhekai@gmail.com>
2023-07-04 14:05:24 +0800

    feat: 🎸 remap key to toggle nvim-tree

--- lua/zhekai/remap.lua
+++ lua/zhekai/remap.lua
@@ -1,7 +1,7 @@
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pb", vim.cmd.NvimTreeFocus)
+vim.keymap.set("n", "<leader>pb", vim.cmd.NvimTreeToggle)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
