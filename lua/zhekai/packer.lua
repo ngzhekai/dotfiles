a7daeb516ee4d77159d08c9dd9ac1831b89f4957
Ng Zhe Kai <ngzhekai@gmail.com>
2023-07-22 11:11:41 +0800

    feat: 🎸 added nvim_comment

--- lua/zhekai/packer.lua
+++ lua/zhekai/packer.lua
@@ -4,46 +4,52 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	-- Using Packer:
-	use 'Mofiqul/dracula.nvim'
-	use({ 
-		'rose-pine/neovim', 
-		as = 'rose-pine' 
-	})
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    -- Using Packer:
+    use 'Mofiqul/dracula.nvim'
+    use({ 
+        'rose-pine/neovim', 
+        as = 'rose-pine' 
+    })
 
-	vim.cmd('colorscheme rose-pine')
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use('theprimeagen/harpoon')
-	use('mbbill/undotree')
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+    vim.cmd('colorscheme rose-pine')
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        requires = {
+            {'JoosepAlviste/nvim-ts-context-commentstring'},
+        }
+    }
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},         -- Required
-			{'hrsh7th/cmp-nvim-lsp'},     -- Required
-			{'hrsh7th/cmp-buffer'},       -- Optional
-			{'hrsh7th/cmp-path'},         -- Optional
-			{'saadparwaiz1/cmp_luasnip'}, -- Optional
-			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},             -- Required
-			{'rafamadriz/friendly-snippets'}, -- Optional
-		}
-	}
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        }
+    }
     use {
         'nvim-tree/nvim-tree.lua',
         requires = {
@@ -53,4 +59,11 @@ return require('packer').startup(function(use)
             require("nvim-tree").setup {}
         end
     }
+    use {
+        'prettier/vim-prettier',
+        run = 'yarn install',
+        ft = {'javascript', 'javascriptreact', 'typescriptreact', 'typescript', 'css', 'less', 'scss', 'graphql', 'markdown', 'vue', 'html'}
+    }
+    use 'terrortylor/nvim-comment'
+    require('nvim_comment').setup()
 end)
